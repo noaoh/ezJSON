80ef73bc82331e46b8dd5ecb0c1ec24982ca73bc
Noah Holt <noahryanholt@gmail.com>
2019-03-21 13:22:10 -0500

    Fixed project structure & added skeletons for unit tests

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ### ezJSON
 A single-file JSON library created by [Michael Gashler](http://gashler.com/mike/)
 Licensed under CC0
+
+Tests are from nst's [JSONTestSuite](https://github.com/nst/JSONTestSuite)
---
+++ src/test/java/com/noaoh/ezJSON/ValidParseTests.java
@@ -0,0 +1,21 @@
+package com.noaoh.ezJSON;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.TestFactory;
+import org.junit.jupiter.api.function.Executable;
+
+public class ValidParseTests {
+
+}
