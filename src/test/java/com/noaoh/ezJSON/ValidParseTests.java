4fe4fcbf54447db33765df87049e67d5e0be9895
Noah Holt <noahryanholt@gmail.com>
2019-03-23 12:04:16 -0500

    Got unit tests to run

--- src/test/java/com/noaoh/ezJSON/ValidParseTests.java
+++ src/test/java/com/noaoh/ezJSON/ValidParseTests.java
@@ -1,21 +1,49 @@
 package com.noaoh.ezJSON;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
+import java.io.File;
+import java.util.*;
+import java.util.concurrent.Callable;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestFactory;
 import org.junit.jupiter.api.function.Executable;
 
 public class ValidParseTests {
+    static HashMap<String, String> testCases;
 
+    @BeforeAll
+    static void initializeJSONFiles() {
+        testCases = new HashMap<String, String>();
+        String currentDir = System.getProperty("user.dir");
+        String jsonPath = "src/test/resources/com/noaoh/ezJSON/test_parsing";
+        File dir = new File(jsonPath);
+        String[] contents = dir.list();
+        String file;
+        for (int x = 0; x < contents.length; x++) {
+            file = contents[x];
+            if (file.startsWith("y")) {
+                StringJoiner joiner = new StringJoiner("/");
+                String path = joiner.add(currentDir).add(jsonPath).add(file).toString();
+                String testName = file.substring(2).replace("_", " ").replace(".json", "");
+                testCases.put(path, testName);
+            }
+        }
+    }
+
+    @Test
+    public void dynamicValidParseTests() {
+        for (Map.Entry<String, String> testCase : testCases.entrySet()) {
+            String path = testCase.getKey();
+            String testName = testCase.getValue();
+            Executable e = () -> Json.load(path);
+            assertDoesNotThrow(e, testName);
+        }
+    }
 }
